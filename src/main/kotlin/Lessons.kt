45a4f1d464acf2c6b31a2d64cd630105c18b1b44
mannonov <jahongir.sh.mannonov@gmail.com>
2021-10-11 14:50:04 +0500

    Avarage

--- src/main/kotlin/Lessons.kt
+++ src/main/kotlin/Lessons.kt
@@ -1,5 +1,9 @@
 class Lessons {
 
+    fun avarage(a: Int, b: Int, c: Int): Int {
 
+        return (a + b + c) / 3
+
+    }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
 
+    val lessons = Lessons()
 
-
+    println(lessons.avarage(4, 4, 4))
 
 }
